6339ef0786e2b729d960cf6864bbdd8c1fc941c0
czhu1217 <cindyzhu0830@gmail.com>
2021-04-23 00:00:11 -0400

    pls

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    public GameObject camera;
+
+    public const float WALK_SPD = 1.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 input = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick);
+        Vector3 movement = camera.transform.TransformDirection(-input.x, 0, -input.y);
+        movement.y = 0;
+        movement = movement.magnitude == 0 ? Vector3.zero : movement / movement.magnitude;
+        movement *= Time.deltaTime * WALK_SPD;
+        this.transform.Translate(movement);
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# VR TCG - A support app for ColorfulCoding VR Tutorials
+
+Main Scene - Hand Tracking
+
+Networking Sample Scene - Photon Unity Network 2 and Photon Voice setup
+
+## Related articles
+ * [Cool Cards & Shader Resources](https://alexanderameye.github.io/)
